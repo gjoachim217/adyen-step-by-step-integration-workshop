3672f911930f663c20680be1a26c41ddd192e1be
gjoachim217 <giovanni.joachim@slalom.com>
2025-12-03 12:50:28 -0500

    Update MainApplication.java

--- src/main/java/com/adyen/workshop/MainApplication.java
+++ src/main/java/com/adyen/workshop/MainApplication.java
@@ -25,9 +25,9 @@ public class MainApplication {
     public void init() {
         log.info("\n----------------------------------------------------------\n\t" +
                 "Application is running on http://localhost:" + applicationConfiguration.getServerPort() +
-                "\nAdyen API Key: " + (applicationConfiguration.getAdyenApiKey() != null) +
-                "\nAdyen Merchant Account: " + (applicationConfiguration.getAdyenMerchantAccount() != null) +
-                "\nAdyen Client Key:" + (applicationConfiguration.getAdyenClientKey() != null) +
+                "\nAdyen API Key: " + (applicationConfiguration.AQFDhmfxLorKbRRAw0m/n3Q5qf3VaY9UCJ16aExb1nSuk2NFisZiGclvMiVgB/7xzYuhmDgJP7cd2UtN0Sh2y3YYVhxAthDBXVsNvuR83LVYjEgiTGAH-LTIFbRELUH7Z9DVBUTvHZkdTPWTCEsTvgJRKnKOGTCo=-i1i*:N*rVV{~6P+eQsK() != null) +
+                "\nAdyen Merchant Account: " + (applicationConfiguration.Merchant account: AdyenPPImplementationTrainingTest_SlalomAtlanta_TEST() != null) +
+                "\nAdyen Client Key:" + (applicationConfiguration.test_GB3JL2RM25FDLABUA5M7NM2PFMCT7B7V() != null) +
                 "\n----------------------------------------------------------");
     }
 }
